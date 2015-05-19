1b754b131967d990cda1c24cabca7afcabb1d491
peterborisov <borisov.peter@yahoo.co.uk>
2015-05-19 21:45:06 +0300

    Играта е готова и започвам с подобрението.

--- SoftUni Project - PingPongGame/PingPongGame.cs
+++ SoftUni Project - PingPongGame/PingPongGame.cs
@@ -3,15 +3,17 @@ using System.Threading;
 
 class Program
 {
-    static int firstPlayerPlankSize = 6;
-    static int secondPlayerPlankSize = 6;
+    static int firstPlayerPlankSize = 8;
+    static int secondPlayerPlankSize = 8;
     static int ballPositionX = Console.WindowWidth / 2;//middle ball position on the console
     static int ballPositionY = Console.WindowHeight / 2;//middle ball position on the console
+    static bool ballUp = true;
+    static bool ballDown = true;
     static int firstPlayerPosition = (Console.WindowHeight / 2) - (firstPlayerPlankSize / 2);//Middle position of the console.
     static int secondPlayerPosition = (Console.WindowHeight / 2) - (firstPlayerPlankSize / 2);//Middle position of the console.
     static int firstPlayerPoints = 0;
     static int secondPlayerPoints = 0;
-    static Random move=new Random();
+    static Random move = new Random();
     static void RemoveScrollBars()
     {
         Console.BufferWidth = Console.WindowWidth;
@@ -41,6 +43,12 @@ class Program
         Console.SetCursorPosition(col, row);
         Console.Write(plankSymbol);
     }
+
+    static void IfLoseBallInTheMiddle()
+    {
+        ballPositionX = Console.WindowWidth / 2;//middle ball position on the console
+        ballPositionY = Console.WindowHeight / 2;//middle ball position on the console
+    }
     static void DrawBall()
     {
         PrintAtPosition(ballPositionX, ballPositionY, '*');
@@ -54,10 +62,10 @@ class Program
 
     static void FirstPlayerDown()
     {
-        if (firstPlayerPosition<Console.WindowHeight-firstPlayerPlankSize)
+        if (firstPlayerPosition < Console.WindowHeight - firstPlayerPlankSize)
         {
-             firstPlayerPosition++;
-        }                 
+            firstPlayerPosition++;
+        }
     }
     static void FirstPlayerUp()
     {
@@ -84,15 +92,90 @@ class Program
 
     static void SecondPlayerMovemend()
     {
-      int randomNum=move.Next(0, 2);
-        if (randomNum == 0)
+        int randomNum = move.Next(0, 2);
+        //if (randomNum == 0)
+        //{
+        //    SecondPlayerUp();
+        //}                                          //random direction
+        //if (randomNum == 1)
+        //{
+        //    SecondPlayerDown();
+        //}
+        if (randomNum == 0)//without this if YOU NEVER WIN.With this if you have 50%
         {
-            SecondPlayerUp();
+            if (ballUp == true) 
+            {
+                SecondPlayerUp();
+            }
+            else
+            {
+                SecondPlayerDown();
+            }
         }
-        if (randomNum == 1)
+
+    }
+
+    static void BallMovemend()
+    {
+        if (ballPositionY == 0)
         {
-            SecondPlayerDown();
+            ballUp = false;
         }
+        if (ballPositionY == Console.WindowHeight - 1)
+        {
+            ballUp = true;
+        }
+        if (ballPositionX == Console.WindowWidth - 1)
+        {
+            IfLoseBallInTheMiddle();
+            ballDown = false;
+            ballUp = true;
+            firstPlayerPoints++;
+            Console.ReadKey();//if someone lose just stop
+        }
+        if (ballPositionX == 0)
+        {
+            IfLoseBallInTheMiddle();
+            ballDown = true;
+            ballUp = true;
+            secondPlayerPoints++;
+            Console.ReadKey();//if someone lose just stop
+        }
+
+
+        if (ballPositionX < 3)
+        {
+            if (ballPositionY >= firstPlayerPosition&&ballPositionY<=firstPlayerPosition+firstPlayerPlankSize)
+            {
+                ballDown = true;
+            }
+        }
+
+        if (ballPositionX >= Console.WindowWidth - 3 - 1)
+        {
+            if (ballPositionY >= secondPlayerPosition && ballPositionY < secondPlayerPosition + secondPlayerPlankSize)
+            {
+                ballDown = false;
+            }
+        }
+
+        if (ballUp)
+        {
+            ballPositionY--;
+        }
+        else
+        {
+            ballPositionY++;
+        }
+        if (ballDown)
+        {
+            ballPositionX++;
+        }
+        else
+        {
+            ballPositionX--;
+        }
+
     }
     static void Main()
     {
@@ -111,6 +194,7 @@ class Program
                     FirstPlayerDown();
                 }
             }
+            BallMovemend();
             SecondPlayerMovemend();
             Console.Clear();
             DrawFirstPlayer();
@@ -118,10 +202,7 @@ class Program
             DrawBall();
             Results();
             Thread.Sleep(60);
-
         }
     }
-
-
 }
 
