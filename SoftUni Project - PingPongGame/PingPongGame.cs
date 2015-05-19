33d0a7661c4ddde5c1e3a33ac61b9b2b6a414091
peterborisov <borisov.peter@yahoo.co.uk>
2015-05-19 20:15:02 +0300

    Пуснах 2-та играча в действие.Единия сме ние ,другия е искуствен интелект и топчето е в средата но още не работи

--- SoftUni Project - PingPongGame/PingPongGame.cs
+++ SoftUni Project - PingPongGame/PingPongGame.cs
@@ -5,10 +5,13 @@ class Program
 {
     static int firstPlayerPlankSize = 6;
     static int secondPlayerPlankSize = 6;
-    static int ballPositionX = 0;
-    static int ballPositionY = 0;
+    static int ballPositionX = Console.WindowWidth / 2;//middle ball position on the console
+    static int ballPositionY = Console.WindowHeight / 2;//middle ball position on the console
     static int firstPlayerPosition = (Console.WindowHeight / 2) - (firstPlayerPlankSize / 2);//Middle position of the console.
     static int secondPlayerPosition = (Console.WindowHeight / 2) - (firstPlayerPlankSize / 2);//Middle position of the console.
+    static int firstPlayerPoints = 0;
+    static int secondPlayerPoints = 0;
+    static Random move=new Random();
     static void RemoveScrollBars()
     {
         Console.BufferWidth = Console.WindowWidth;
@@ -38,16 +41,87 @@ class Program
         Console.SetCursorPosition(col, row);
         Console.Write(plankSymbol);
     }
+    static void DrawBall()
+    {
+        PrintAtPosition(ballPositionX, ballPositionY, '*');
+    }
+
+    static void Results()
+    {
+        Console.SetCursorPosition(Console.WindowWidth / 2 - 1, 0);
+        Console.Write("{0}-{1}", firstPlayerPoints, secondPlayerPoints);
+    }
+
+    static void FirstPlayerDown()
+    {
+        if (firstPlayerPosition<Console.WindowHeight-firstPlayerPlankSize)
+        {
+             firstPlayerPosition++;
+        }                 
+    }
+    static void FirstPlayerUp()
+    {
+        if (firstPlayerPosition > 0)
+        {
+            firstPlayerPosition--;
+        }
+
+    }
+    static void SecondPlayerDown()
+    {
+        if (secondPlayerPosition < Console.WindowHeight - secondPlayerPlankSize)
+        {
+            secondPlayerPosition++;
+        }
+    }
+    static void SecondPlayerUp()
+    {
+        if (secondPlayerPosition > 0)
+        {
+            secondPlayerPosition--;
+        }
+    }
+
+    static void SecondPlayerMovemend()
+    {
+      int randomNum=move.Next(0, 2);
+        if (randomNum == 0)
+        {
+            SecondPlayerUp();
+        }
+        if (randomNum == 1)
+        {
+            SecondPlayerDown();
+        }
+    }
     static void Main()
     {
         RemoveScrollBars();
         while (true)
         {
+            if (Console.KeyAvailable)
+            {
+                ConsoleKeyInfo keys = Console.ReadKey();
+                if (keys.Key == ConsoleKey.UpArrow)
+                {
+                    FirstPlayerUp();
+                }
+                if (keys.Key == ConsoleKey.DownArrow)
+                {
+                    FirstPlayerDown();
+                }
+            }
+            SecondPlayerMovemend();
+            Console.Clear();
             DrawFirstPlayer();
             DrawSecondPlayer();
-            Console.WriteLine();
+            DrawBall();
+            Results();
             Thread.Sleep(60);
+
         }
     }
+
+
 }
 
