f8376a0cf7c8ebf861d6cca022474c238569b6bd
tutzy <tatqna.miteva@gmail.com>
2015-05-19 15:15:33 +0300

    Added RemoveScrollBars method.

--- SoftUni Project - PingPongGame/PingPongGame.cs
+++ SoftUni Project - PingPongGame/PingPongGame.cs
@@ -2,9 +2,16 @@
 
     class Program
     {
+        static void RemoveScrollBars()
+        {
+            Console.BufferWidth = Console.WindowWidth;
+            Console.BufferHeight = Console.WindowHeight;
+        }
+
         static void Main()
         {
-
+            RemoveScrollBars();
+      
         }
     }
 
